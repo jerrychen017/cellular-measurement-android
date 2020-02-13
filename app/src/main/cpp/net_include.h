3eb01e226630e53594a48d872ae3b8a4fbc0cca9
Guangrui Chen <gchen41@jhu.edu>
2020-02-13 00:28:28 -0500

    add c source and header files

---
+++ app/src/main/cpp/net_include.h
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <limits.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <errno.h>
+
+#include <sys/time.h>
+
+#define BUFF_SIZE 1000
+#define TIMEOUT_SEC 2
+#define TIMEOUT_USEC 0
